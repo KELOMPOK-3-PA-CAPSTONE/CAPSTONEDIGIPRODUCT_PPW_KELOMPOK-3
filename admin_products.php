0a4e037832639e92a3cf17587eb566ea953ac21c
AufaTriHapsari <122031507+AufaTriHapsari@users.noreply.github.com>
2024-05-09 19:46:45 +0700

    Update.php

--- admin_products.php
+++ admin_products.php
@@ -3,22 +3,13 @@ include 'config.php';
 
 session_start();
 
-// Periksa apakah pengguna sudah login dan memiliki peran admin
-$admin_id = isset($_SESSION['admin_id']) ? $_SESSION['admin_id'] : null;
-$admin_role = isset($_SESSION['admin_role']) ? $_SESSION['admin_role'] : null;
-
-// cek biar admin aj yg login
-if (!$admin_id || $admin_role !== 'admin') {
-    header('location:login.php');
-    exit;
-}
-
-
 $admin_id = $_SESSION['admin_id'];
 
 if (!isset($admin_id)) {
     header('location:login.php');
 }
+
+$message = array();
 
 if (isset($_POST['add_product'])) {
     $name = mysqli_real_escape_string($conn, $_POST['name']);
@@ -33,25 +24,29 @@ if (isset($_POST['add_product'])) {
     $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name'") or die('query failed');
 
     if (mysqli_num_rows($select_product_name) > 0) {
-        $message[] = 'Product name already added';
+        $message[] = 'Nama produk sudah ada';
     } else {
-        $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, deskripsi, tujuan, image) VALUES('$name', '$price', '$deskripsi', '$tujuan', '$image')") or die('query failed');
-
-        if ($add_product_query) {
-            if ($image_size > 2000000) {
-                $message[] = 'Image size is too large';
-            } else {
-                move_uploaded_file($image_tmp_name, $image_folder);
-                $message[] = 'Product added successfully!';
-            }
+        if ($price > 200000) {
+            $message[] = 'Harga produk tidak boleh melebihi Rp200.000';
         } else {
-            $message[] = 'Product could not be added!';
+            $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, deskripsi, tujuan, image) VALUES('$name', '$price', '$deskripsi', '$tujuan', '$image')") or die('query failed');
+
+            if ($add_product_query) {
+                if ($image_size > 2000000) {
+                    $message[] = 'Ukuran gambar terlalu besar';
+                } else {
+                    move_uploaded_file($image_tmp_name, $image_folder);
+                    $message[] = 'Produk berhasil ditambahkan!';
+                }
+            } else {
+                $message[] = 'Produk tidak bisa ditambahkan!';
+            }
         }
     }
 }
 
-if (isset($_GET['delete'])) {
-    $delete_id = $_GET['delete'];
+if (isset($_POST['delete_product'])) {
+    $delete_id = $_POST['delete_id'];
     $delete_image_query = mysqli_query($conn, "SELECT image FROM `products` WHERE id = '$delete_id'") or die('query failed');
     $fetch_delete_image = mysqli_fetch_assoc($delete_image_query);
     unlink('uploaded_img/' . $fetch_delete_image['image']);
@@ -76,7 +71,7 @@ if (isset($_POST['update_product'])) {
 
     if (!empty($update_image)) {
         if ($update_image_size > 2000000) {
-            $message[] = 'Image file size is too large';
+            $message[] = 'Ukuran file gambar terlalu besar';
         } else {
             mysqli_query($conn, "UPDATE `products` SET image = '$update_image' WHERE id = '$update_p_id'") or die('query failed');
             move_uploaded_file($update_image_tmp_name, $update_folder);
@@ -96,7 +91,7 @@ if (isset($_POST['update_product'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
+    <title>Produk</title>
 
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -110,27 +105,35 @@ if (isset($_POST['update_product'])) {
 
     <?php include 'admin_header.php'; ?>
 
-    <!-- product CRUD section starts -->
+    <!-- bagian CRUD produk dimulai -->
 
     <section class="add-products">
 
-        <h1 class="title">Shop Products</h1>
+        <h1 class="title">Produk Toko</h1>
+
+        <?php
+        if (!empty($message)) {
+            echo '<div class="message">';
+            foreach ($message as $msg) {
+                echo '<p>' . $msg . '</p>';
+            }
+            echo '</div>';
+        }
+        ?>
 
         <form action="" method="post" enctype="multipart/form-data">
-            <h3>Add Product</h3>
-            <input type="text" name="name" class="box" placeholder="Enter Product Name" required>
-            <input type="number" min="0" name="price" class="box" placeholder="Enter Product Price" required>
-            <textarea name="deskripsi" class="box" placeholder="Enter Product Description" required></textarea>
-            <textarea name="tujuan" class="box" placeholder="Enter Product Goal" required></textarea>
+            <h3>Tambah Produk</h3>
+            <input type="text" name="name" class="box" placeholder="Masukkan Nama Produk" required>
+            <input type="text" name="price" class="box" value=".000" required>
+            <textarea name="deskripsi" class="box" placeholder="Masukkan Deskripsi Produk" required></textarea>
+            <textarea name="tujuan" class="box" placeholder="Masukkan Tujuan Produk" required></textarea>
             <input type="file" name="image" accept="image/jpg, image/jpeg, image/png" class="box" required>
-            <input type="submit" value="Add Product" name="add_product" class="btn">
+            <input type="submit" value="Tambah Produk" name="add_product" class="btn">
         </form>
 
     </section>
 
-  
-
-    <!-- show products -->
+    <!-- tampilkan produk -->
 
     <section class="show-products">
 
@@ -145,13 +148,16 @@ if (isset($_POST['update_product'])) {
                         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
                         <div class="name"><?php echo $fetch_products['name']; ?></div>
                         <div class="price">Rp<?php echo $fetch_products['price']; ?>.000</div>
+                        <form action="" method="post">
+                            <input type="hidden" name="delete_id" value="<?php echo $fetch_products['id']; ?>">
+                            <button type="submit" name="delete_product" class="delete-btn" onclick="return confirm('Hapus produk ini?');">Hapus</button>
+                        </form>
                         <a href="admin_products.php?update=<?php echo $fetch_products['id']; ?>" class="option-btn">Update</a>
-                        <a href="admin_products.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-btn" onclick="return confirm('Delete this product?');">Delete</a>
                     </div>
             <?php
                 }
             } else {
-                echo '<p class="empty">No products added yet!</p>';
+                echo '<p class="empty">Belum ada produk ditambahkan!</p>';
             }
             ?>
         </div>
@@ -171,14 +177,13 @@ if (isset($_POST['update_product'])) {
                         <input type="hidden" name="update_p_id" value="<?php echo $fetch_update['id']; ?>">
                         <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['image']; ?>">
                         <img src="uploaded_img/<?php echo $fetch_update['image']; ?>" alt="">
-                        <input type="text" name="update_name" value="<?php echo $fetch_update['name']; ?>" class="box" required placeholder="Enter Product Name">
-                        <input type="number" name="update_price" value="<?php echo $fetch_update['price']; ?>" min="0" class="box" required placeholder="Enter Product Price">
-                        <!-- Tambahkan input untuk deskripsi dan tujuan -->
-                        <textarea name="update_deskripsi" class="box" required placeholder="Enter Product Description"><?php echo $fetch_update['deskripsi']; ?></textarea>
-                        <textarea name="update_tujuan" class="box" required placeholder="Enter Product Goal"><?php echo $fetch_update['tujuan']; ?></textarea>
+                        <input type="text" name="update_name" value="<?php echo $fetch_update['name']; ?>" class="box" required placeholder="Masukkan Nama Produk">
+                        <input type="text" name="update_price" value="<?php echo $fetch_update['price']; ?>" class="box" placeholder="Masukkan Harga Produk" required>
+                        <textarea name="update_deskripsi" class="box" required placeholder="Masukkan Deskripsi Produk"><?php echo $fetch_update['deskripsi']; ?></textarea>
+                        <textarea name="update_tujuan" class="box" required placeholder="Masukkan Tujuan Produk"><?php echo $fetch_update['tujuan']; ?></textarea>
                         <input type="file" class="box" name="update_image" accept="image/jpg, image/jpeg, image/png">
                         <input type="submit" value="Update" name="update_product" class="btn">
-                        <input type="reset" value="Cancel" id="close-update" class="option-btn">
+                        <input type="reset" value="Batal" id="close-update" class="option-btn">
                     </form>
         <?php
                 }
